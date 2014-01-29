1092f9e9f6d343bc61c5120b4fc581b1053febc3
trippel <cpeddecord@gmail.com>
2014-01-28 21:24:23 -0800

    Homework Submission

--- ChrisPedd/cash-register.js
+++ ChrisPedd/cash-register.js
@@ -1,10 +1,24 @@
 var cashRegister = {
-  // implement me!
+	total: 0,
+	add: function (cashMonies) {
+		this.total += cashMonies;
+		console.log("You've added $" + cashMonies + " to the register.");
+	},
+	subtract: function (cashMonies) {
+		this.total -= cashMonies;
+		console.log("You've taken $" + cashMonies + " out of the register.");
+	},
 };
+
+console.log("\n--- Welcome to the Cash Register! ---\n")
 
 cashRegister.add(1.25);
 cashRegister.add(3.63);
 cashRegister.add(4.99);
 cashRegister.subtract(1.15);
 
-console.log("Your total is " + cashRegister.total());
+
+if (cashRegister.total >= 100) {
+	console.log ("wow. very cash. much dollar: $" + cashRegister.total + ". wow.");
+	} else { console.log("Your register total is $" + cashRegister.total + ".");
+}
--- ChrisPedd/shopping-cart.js
+++ ChrisPedd/shopping-cart.js
@@ -1,36 +1,79 @@
 var shoppingCart = {
-  // implement me!
+	add: function (itemName, itemCount) {
+		for (i=0; i<this.cart.length; i++) {
+		     if (this.cart[i].name == itemName) {
+		        this.cart[i].subCount += itemCount;
+		     }
+		   }
+	},
+	remove: function (itemName, itemCount) {
+		for (i=0; i<this.cart.length; i++) {
+		     if (this.cart[i].name == itemName) {
+		        this.cart[i].subCount -= itemCount;
+		     }
+		   }
+	},
+	list: function () {
+		console.log("--- Shopping Cart:");
+		this.cart.forEach(function (thing) {
+			if (thing.subCount >0 ) {
+				console.log (thing.subCount + " X " + thing.description);
+			}
+		});
+		console.log("\n");
+	},
+	total: function () {
+		console.log("--- Grand Total:")
+		var grandTotal = 0;
+		this.cart.forEach(function (thing) {
+			//debugging line to test for proper item checks 
+			//console.log(thing.subCount + "|" + thing.price);
+			grandTotal += thing.price * thing.subCount;
+		});
+		console.log("$" + grandTotal);
+	},
+
+	
+	///////////////////// Very Shopping Cart, Such Array /////////////////////////
+	
+	cart: [
+		{
+		name: "item1",
+		description: "Huggies Much Doge Diapers",
+		price: 19.77,
+		subCount: 0,
+		},
+	    {
+	    name: "item2",
+		description: "Tylenol Extra Strength Methamphetamines",
+		price: 22.38,
+		subCount: 0,
+		},
+		{
+		name: "item3",
+		description: "Flents Easy Listening Jazzy Foam Ear Plugs",
+	    price: 11.50,
+	    subCount: 0,
+		},
+		{
+		name: "item4",
+		description: "Monsters Eat Whiny Children for Breakfast",
+	    price: 11.07,
+	    subCount: 0,
+		},
+	]
 };
 
-var item1 = shoppingCart.add({description: "Huggies Little Snugglers Diapers",
-                              count: 4,
-                              price: 19.77});
 
-var item2 = shoppingCart.add({description: "Tylenol Extra Strength Acetaminophen",
-                              count: 2,
-                              price: 22.38});
+console.log("\n*** WELCOME TO B.V. MART ***\n")
+// had to change arguments to strings to make it work properly within my array, would love to know why
+shoppingCart.add("item1", 4);
+shoppingCart.add("item2", 2);
+shoppingCart.add("item3", 10);
+shoppingCart.add("item4", 1);
 
-var item3 = shoppingCart.add({description: "Flents Quiet Please Foam Ear Plugs",
-                              count: 10,
-                              price: 11.50});
+shoppingCart.remove("item3", 6);
+shoppingCart.remove("item4", 1);
 
-var item4 = shoppingCart.add({description: "Monsters Eat Whiny Children",
-                              count: 1,
-                              price: 11.07});
-
-shoppingCart.remove(item3, 6);
-shoppingCart.remove(item4, 1);
-
-console.log(shoppingCart.list());
-
-/* Should print:
-[
-'4 X Huggies Little Snugglers Diapers',
-'2 X Tylenol Extra Strength Acetaminophen',
-'4 X Flents Quiet Please Foam Ear Plugs',
-]
-*/
-
-console.log(shoppingCart.total());
-
-// Should print: 169.84
+shoppingCart.list();
+shoppingCart.total();
