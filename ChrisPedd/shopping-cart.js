5dd2c7f6aec8177fe3e109ae667f3790f03a6c75
trippel <cpeddecord@gmail.com>
2014-01-29 08:00:00 -0800

    Moved products out of shoppingCart{} and into products[], re-wrote shoppingCart.add to move from products[] to shoppingCart.cart[]

--- ChrisPedd/shopping-cart.js
+++ ChrisPedd/shopping-cart.js
@@ -1,11 +1,18 @@
 var shoppingCart = {
+	cart: [],
+	grandTotal: 0,
 	add: function (itemName, itemCount) {
+		for (i=0; i<products.length; i++) {
+		     if (products[i].name == itemName) {
+		        this.cart.push(products[i]);      
+		     }
+		}
 		for (i=0; i<this.cart.length; i++) {
 		     if (this.cart[i].name == itemName) {
 		        this.cart[i].subCount += itemCount;
 		        console.log("++Added " + itemCount + " X " + this.cart[i].description.substring(0,20) + "... to the shopping cart++");
 		     }
-		   }
+		}
 	},
 	remove: function (itemName, itemCount) {
 		for (i=0; i<this.cart.length; i++) {
@@ -23,49 +30,46 @@ var shoppingCart = {
 				console.log (thing.subCount + " X " + thing.description);
 			}
 		});
-		console.log("\n");
 	},
 	total: function () {
-		console.log("--- Grand Total:")
-		var grandTotal = 0;
+		var subTotal = 0;
 		this.cart.forEach(function (thing) {
 			//debugging line to test for proper item checks 
 			//console.log(thing.subCount + "|" + thing.price);
-			grandTotal += thing.price * thing.subCount;
+			subTotal += thing.price * thing.subCount;
 		});
-		console.log("$" + grandTotal);
+		this.grandTotal += subTotal;
+		console.log("\n--- Grand Total:\n" + this.grandTotal);
 	},
+};
 
-	
-	///////////////////// Very Shopping Cart, Such Array /////////////////////////
-	
-	cart: [
-		{
+products = [
+		item1 = {
 		name: "item1",
 		description: "Huggies Much Doge Diapers",
 		price: 19.77,
 		subCount: 0,
 		},
-	    {
+	    item2 = {
 	    name: "item2",
 		description: "Tylenol Extra Strength Methamphetamines",
 		price: 22.38,
 		subCount: 0,
 		},
-		{
+		item3 = {
 		name: "item3",
 		description: "Flents Easy Listening Jazzy Foam Ear Plugs",
 	    price: 11.50,
 	    subCount: 0,
 		},
-		{
+		item4 = {
 		name: "item4",
 		description: "Monsters Eat Whiny Children for Breakfast",
 	    price: 11.07,
 	    subCount: 0,
 		},
-	]
-};
+
+];
 
 
 console.log("\n*** WELCOME TO B.V. MART ***\n")
