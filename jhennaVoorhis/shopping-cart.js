19a11cb282063c52700ac319be80196b990f9cd7
Jhenna Voorhis <jhenna.voorhis@gmail.com>
2014-01-29 14:12:57 -0800

    fixed shopping-cart.js to meet assignment

--- jhennaVoorhis/shopping-cart.js
+++ jhennaVoorhis/shopping-cart.js
@@ -1,29 +1,37 @@
 var shoppingCart = {
   itemArray: [], //an array to hold the items in the cart
-  priceTotal: 0, //keeps track of total price of items in cart
+  ssn: 0, //unique id
   add: function(item){
   	var thingType = typeof item; //will be used to ensure things added are objects with a description, count, and price
   	if (thingType=== 'object' && item.description!==undefined && item.count!==undefined && item.price !== undefined){
-			this.itemArray.push(item); //adds item to cart
-  			this.priceTotal += item.count * item.price; //adds cost of item(s) depending on quantity
-  			console.log(item.count + "X of " + item.description + " successfully added to your cart!"); //friendly message that item was added with quantity added
-  			return item; //stores the item reference to the unique id given in the item declaration
-		};
-  },
-  remove: function(id, qt){
-  	var index = this.itemArray.indexOf(id); //locates the item within the cart
-  	if (index >= 0 && qt >= id.count){ //if the item is in the cart and more than or exactly the amount of the item in the cart is removed, will remove whole item from cart
-			this.priceTotal -= id.count * id.price;
-			this.itemArray.splice(index, 1);
-			console.log(id.description + " was entirely removed from your cart");
-		} else if (index >= 0 && qt < id.count){ //if item is in cart and don't want to remove all of it, adjusts price and remaining quantity according to how many want to be removed
-			this.priceTotal -= qt * id.price;
-			id.count -= qt;
-			console.log(qt + "X of " + id.description + " removed from cart, " + id.count + "X of it remain.");
-		} else { //if item is not in cart at all gives friendly message to that effect
-			console.log("I'm sorry, that item does not appear to be in your cart so it cannot be removed.");
+			item.ssn = this.ssn; //assigns unique id and then steps id
+  			this.ssn++;
+  			this.itemArray.push(item); //adds item to cart
+  			
+  			//console.log(item.count + "X of " + item.description + " successfully added to your cart!"); //friendly message that item was added with quantity added
+  			
+  			return item.ssn; //stores the item reference to the unique id given in the item declaration
+		} else {
+			console.log("That item could not be added to the cart, please double check it is a valid item.");
 		}
   },
+
+  remove: function (id, qt) {
+  	this.itemArray.forEach(function (object, index, array) {
+  		if (object.ssn===id){
+  			if (qt >= object.count){ //removes item entirely based on qt given
+  				array.splice(index, 1);
+  				// console.log(object.description + " was entirely removed from your cart");
+  			} else if (qt < object.count && qt > 0){ //removes specified qt
+  				object.count -= qt;
+  				// console.log(qt + "X of " + object.description + " removed from cart, " + object.count + "X of it remain.");
+  			} else {
+  				console.log("Could not perform request, please double check that specified item and quantity are valid.");
+  			}
+  		}
+  	});
+  },
+
   list: function (){
   	var listArr = [];
   	this.itemArray.forEach(function (object) { //accesses value of each item in the shopping cart through the values stored in itemArray
@@ -32,7 +40,11 @@ var shoppingCart = {
   	return listArr;
   },
   total: function () {
-  	return this.priceTotal;
+  	var priceTotal = 0;
+  	this.itemArray.forEach(function (object){
+  		priceTotal += object.count * object.price;
+  	})
+  	return priceTotal;
   }
 };
 
